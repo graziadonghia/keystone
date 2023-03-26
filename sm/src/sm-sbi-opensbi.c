9b0b709dfdd48f83c136b0dee827be5d34fa79fa
Giacomo <s301311@studenti.polito.it>
2023-03-26 17:09:32 +0200

    Added debug prints in SM-sbi_ecall_keystone_enclave_handler

--- sm/src/sm-sbi-opensbi.c
+++ sm/src/sm-sbi-opensbi.c
@@ -14,6 +14,8 @@
 #include "sm.h"
 #include "cpu.h"
 
+#define SM_DEBUG_PRINT 0
+
 static int sbi_ecall_keystone_enclave_handler(unsigned long extid, unsigned long funcid,
                      const struct sbi_trap_regs *regs,
                      unsigned long *out_val,
@@ -35,38 +37,68 @@ static int sbi_ecall_keystone_enclave_handler(unsigned long extid, unsigned long
 
   switch (funcid) {
     case SBI_SM_CREATE_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Create enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_create_enclave(out_val, regs->a0);
       break;
     case SBI_SM_DESTROY_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Destroy enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_destroy_enclave(regs->a0);
       break;
     case SBI_SM_RUN_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Run enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_run_enclave((struct sbi_trap_regs*) regs, regs->a0);
       __builtin_unreachable();
       break;
     case SBI_SM_RESUME_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Resume enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_resume_enclave((struct sbi_trap_regs*) regs, regs->a0);
       __builtin_unreachable();
       break;
     case SBI_SM_RANDOM:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Random (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       *out_val = sbi_sm_random();
       retval = 0;
       break;
     case SBI_SM_ATTEST_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Attest enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_attest_enclave(regs->a0, regs->a1, regs->a2);
       break;
     case SBI_SM_GET_SEALING_KEY:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Get sealing key (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_get_sealing_key(regs->a0, regs->a1, regs->a2);
       break;
     case SBI_SM_STOP_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Stop enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_stop_enclave((struct sbi_trap_regs*) regs, regs->a0);
       __builtin_unreachable();
       break;
     case SBI_SM_EXIT_ENCLAVE:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Exit enclave (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_exit_enclave((struct sbi_trap_regs*) regs, regs->a0);
       __builtin_unreachable();
       break;
     case SBI_SM_CALL_PLUGIN:
+      #if SM_DEBUG_PRINT
+      sbi_printf("D[SM] Call plugin (sm/sm-sbi-opensbi.c)\r\n");
+      #endif
       retval = sbi_sm_call_plugin(regs->a0, regs->a1, regs->a2, regs->a3);
       break;
     default:
